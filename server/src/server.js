56780568415077f13beb7c1e37fbf9a9bf8f401d
John Ibsen <jibsen@indigoag.com>
2019-11-24 20:45:36 -0500

    Dockerize the development workflow

--- server/src/server.js
+++ server/src/server.js
@@ -19,7 +19,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/transactions', transactionsApi);
 
-mongoose.connect('mongodb://localhost', {dbName:'iwab', replicaSet:'rs', useNewUrlParser: true});
+const mongo_hostname = (process.env.MONGO_HOSTNAME || 'localhost');
+const mongo_port = (process.env.MONGO_PORT || 27017);
+
+console.log('test ');
+
+mongoose.connect(`mongodb://${mongo_hostname}:${mongo_port}`, {dbName:'iwab', useNewUrlParser: true});
 
 const db = mongoose.connection;
 
